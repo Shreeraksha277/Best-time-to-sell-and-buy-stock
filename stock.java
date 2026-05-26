614ad79da551e5b05ccea5fa20330214da462b00
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-05-26 21:15:49 +0530

    Update stock.java

--- stock.java
+++ stock.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int maxProfit(int[] prices) {
+   class Solution {
+     public int maxProfit(int[] prices) {
         int min = Integer.MAX_VALUE;
         int max=0;
         for(int i=0;i<prices.length;i++)
